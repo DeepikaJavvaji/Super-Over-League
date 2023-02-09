0c87e233fd9a33dc37edabc702ae1cde04f966d1
DeepikaJavvaji <deepika.javvaji36@gmail.com>
2023-02-09 14:25:55 +0000

    reactjs first coding assignment

---
+++ index.js
@@ -0,0 +1,18 @@
+const element = (
+  //   Write your code here.
+  <div className="container">
+    <h1>Super Overleague</h1>
+    <div>
+      <img
+        className="img"
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+      />
+      <img
+        className="img"
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
